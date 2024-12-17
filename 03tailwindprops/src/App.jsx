2d629da7b97103f99908b264e8c04530ff0a6ec7
AbdulRahmanAraka <mailing2rahman@gmail.com>
2024-12-17 21:39:08 +0530

    props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-green-400 p-4 text-black rounded'>Tailwind Test</h1>
-      
+      <h1 className='bg-green-400 p-4 text-black rounded mb-4'>Tailwind Test</h1>
+      <Card userName="Abdul" btnText="Explore"/>
+      <Card userName="Araka" btnText="Explore more"/>
+      <Card userName="Ahsan" />
+
+
     </>
   )
 }
